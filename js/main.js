d298fbd1a38fd5f69e345669f24784159c32e0a7
nekores <sajidxullah@gmail.com>
2019-02-05 19:40:51 +0500

    configured Project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("this is main.js")
+// // Add slideDown animation to Bootstrap dropdown when expanding.
+// $('.dropdown').on('show.bs.dropdown', function () {
+//     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+// });
+
+// // Add slideUp animation to Bootstrap dropdown when collapsing.
+// $('.dropdown').on('hide.bs.dropdown', function () {
+//     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+// });
