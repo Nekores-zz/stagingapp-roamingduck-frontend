5f190d62c0da18b9a56e4caba38b4814e7c0e338
nekores <sajidxullah@gmail.com>
2019-02-07 18:45:56 +0500

    80% worked on Trip Dashboard

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-// // Add slideDown animation to Bootstrap dropdown when expanding.
-// $('.dropdown').on('show.bs.dropdown', function () {
-//     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-// });
+$('.btn-bottomMenuGroup').on('click',()=>{
+    $('.buttonGroupMenuDropUp ul').toggleClass("wow");
 
-// // Add slideUp animation to Bootstrap dropdown when collapsing.
-// $('.dropdown').on('hide.bs.dropdown', function () {
-//     $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-// });
+})
