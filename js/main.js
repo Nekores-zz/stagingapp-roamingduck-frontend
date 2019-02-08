d4caf29622619c982a0e95fe0ac57170bc887d43
nekores <sajidxullah@gmail.com>
2019-02-08 19:30:21 +0500

    worked on iternary secton and done the changes too

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $('.btn-bottomMenuGroup').on('click',()=>{
-    $('.buttonGroupMenuDropUp ul').toggleClass("wow");
-
+    $('.buttonGroupMenuDropUp ul').toggleClass("showMenudropUp");
+    // if ($('.bottomMenuGroup .buttonGroupMenuDropUpForm').toggleClass("showMenudropUpForm") == 1) {
+    //     alert(1);
+    // }
+    
+});
+$('.buttonGroupMenuDropUp  ul li a').on('click',()=>{
+    $('.menubox .buttonGroupMenuDropUpForm').toggleClass("showMenudropUpForm");
+    $('.buttonGroupMenuDropUp ul').toggleClass("showMenudropUp");
+    
+})
+$('.buttonGroupMenuDropUpForm button.closeForm').on('click',()=>{
+    $('.menubox .buttonGroupMenuDropUpForm').toggleClass("showMenudropUpForm");
+    $('.buttonGroupMenuDropUp ul').toggleClass("showMenudropUp");
 })
