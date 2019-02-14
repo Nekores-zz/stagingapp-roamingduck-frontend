cda933ead16a192abbd6a3ab50e7ee33c2172e0a
nekores <sajidxullah@gmail.com>
2019-02-14 23:27:11 +0500

    added popup form

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 $('.btn-bottomMenuGroup').on('click',()=>{
-    $('.buttonGroupMenuDropUp ul').toggleClass("showMenudropUp");
-    // if ($('.bottomMenuGroup .buttonGroupMenuDropUpForm').toggleClass("showMenudropUpForm") == 1) {
-    //     alert(1);
-    // }
-    
+    $('.buttonGroupMenuDropUp ul').toggleClass("showMenudropUp"); 
 });
 $('.buttonGroupMenuDropUp  ul li a').on('click',()=>{
     $('.menubox .buttonGroupMenuDropUpForm').toggleClass("showMenudropUpForm");
     $('.buttonGroupMenuDropUp ul').toggleClass("showMenudropUp");
     
 })
-$('.buttonGroupMenuDropUpForm button.closeForm').on('click',()=>{
+$('.buttonGroupMenuDropUpForm input.closeForm , .buttonGroupMenuDropUpForm button.close.closeBtnCross').on('click',()=>{
     $('.menubox .buttonGroupMenuDropUpForm').toggleClass("showMenudropUpForm");
     $('.buttonGroupMenuDropUp ul').toggleClass("showMenudropUp");
 })
@@ -37,21 +33,18 @@ $(document).ready(function () {
 
                 // TUTORIAL 1: ON PRESS BACK BUTTON
                 $('#navbarSectionTutorPopupBack').on('click', () => {
-                    // $('.navbarSectionT?utorPopup-body').removeClass('navbarSectionTutorPopup-body-active');
-                    // $('.tutorWrapper').removeClass('TutorActive');
                     $('#navbarSection').removeClass('indexShow');
                     $('.navbarSectionTutorPopup-body').removeClass('navbarSectionTutorPopup-body-active');
-                    // $('.bottomMenuGroupTutorPopup-body').addClass('bottomMenuGroupTutorPopup-body-active');
-                    // $('#tutorBoxOne').addClass('indexShow');
+                    
                     tutorial1();
-                    // alert(1)
+                    
                 });
                 // TUTORIAL 1: ON PRESS FINISH BUTTON
                 $('#navbarSectionTutorPopupFinish').on('click', () => {
                     $('#navbarSection').removeClass('indexShow');
                     $('.navbarSectionTutorPopup-body').removeClass('navbarSectionTutorPopup-body-active');
                     $('.tutorWrapper').removeClass('TutorActive');
-                    // tutorial3();
+                   
                 });
             }
 
@@ -143,4 +136,95 @@ $(document).ready(function () {
     })
 
 });
+
+
+
+
+
+
+// 
+
+
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function () {
+    if (animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+
+    //activate next step on progressbar using the index of next_fs
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({ opacity: 0 }, {
+        step: function (now, mx) {
+            //as the opacity of current_fs reduces to 0 - stored in "now"
+            //1. scale current_fs down to 80%
+            scale = 1 - (1 - now) * 0.2;
+            //2. bring next_fs from the right(50%)
+            left = (now * 50) + "%";
+            //3. increase opacity of next_fs to 1 as it moves in
+            opacity = 1 - now;
+            current_fs.css({
+                'transform': 'scale(' + scale + ')',
+                'position': 'absolute'
+            });
+            next_fs.css({ 'left': left, 'opacity': opacity });
+        },
+        duration: 800,
+        complete: function () {
+            current_fs.hide();
+            animating = false;
+        },
+        //this comes from the custom easing plugin
+        easing: 'easeInOutBack'
+    });
+});
+
+$(".previous").click(function () {
+    if (animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+
+    //de-activate current step on progressbar
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+    //show the previous fieldset
+    previous_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({ opacity: 0 }, {
+        step: function (now, mx) {
+            //as the opacity of current_fs reduces to 0 - stored in "now"
+            //1. scale previous_fs from 80% to 100%
+            scale = 0.8 + (1 - now) * 0.2;
+            //2. take current_fs to the right(50%) - from 0%
+            left = ((1 - now) * 50) + "%";
+            //3. increase opacity of previous_fs to 1 as it moves in
+            opacity = 1 - now;
+            current_fs.css({ 'left': left });
+            previous_fs.css({ 'transform': 'scale(' + scale + ')', 'opacity': opacity });
+        },
+        duration: 800,
+        complete: function () {
+            current_fs.hide();
+            animating = false;
+        },
+        //this comes from the custom easing plugin
+        easing: 'easeInOutBack'
+    });
+});
+
+$(".submit").click(function () {
+    // return false;
+})
+
 
